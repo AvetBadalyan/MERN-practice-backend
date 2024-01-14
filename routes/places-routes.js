36ce97fa4bd802ac08ea0e5e5fc96cbc84387a2a
AvetBadalyan <avetbadalyan@gmail.com>
2024-01-14 22:19:42 +0400

    add an error handling class

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const HttpError = require("./../models/http-error");
+
 const router = express.Router();
 
 const DUMMY_PLACES = [
@@ -21,13 +23,21 @@ router.get("/:pid", (req, res, next) => {
   const place = DUMMY_PLACES.find((p) => {
     return p.id === placeId;
   });
+
+  if (!place) {
+    throw new HttpError("Could not find a place for the provided id", 404);
+  }
+
   res.json({ place });
 });
 
 router.get("/user/:uid", (req, res, next) => {
   const userId = req.params.uid;
-  console.log(userId);
   const place = DUMMY_PLACES.find((place) => place.creator === userId);
+
+  if (!place) {
+    next(new HttpError("Could not find a place for the provided id", 404));
+  }
 
   res.json({ place });
 });
