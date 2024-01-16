475bcd7fa176a8a71d7b8e85167a4337ae04ebe0
AvetBadalyan <avetbadalyan@gmail.com>
2024-01-17 00:57:49 +0400

    get places for userid from mongo

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -40,18 +40,25 @@ const getPlaceById = async (req, res, next) => {
   res.json({ place: place.toObject({ getters: true }) });
 };
 
-const getPlacesByUserId = (req, res, next) => {
+const getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid;
-  const places = DUMMY_PLACES.filter((place) => {
-    return place.creator === userId;
-  });
+
+  let places;
+  try {
+    places = await Place.find({ creator: userId });
+  } catch (err) {
+    const error = new HttpError("Fetching places failed", 500);
+    return next(error);
+  }
 
   if (!places.length) {
     return next(
       new HttpError("Could not find places for the provided user id.", 404)
     );
   }
-  res.json({ places });
+  res.json({
+    places: places.map((place) => place.toObject({ getters: true })),
+  });
 };
 
 const createPlace = async (req, res, next) => {
